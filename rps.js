d84503debad2b47efc78bd2ac059ffb5f1d8166d
shinyfuwante <mr.philliptang@gmail.com>
2022-04-05 19:36:13 -0700

    Create a basic UI for RPS
    Very rough UI for RPS that lets one play vs computer and will print a scoreboard when someone reaches 5.
    There is no reset and no locking out of the buttons once someone hits 5.

--- rps.js
+++ rps.js
@@ -5,7 +5,8 @@ const SCISSORS = 2;
 function parseChoice(selection) {
     if (typeof(selection) == "number" && selection < 3) return selection;
     if (typeof(selection) == "string") {
-        selection = selection.toLowerCase();
+        selection = selection.toLowerCase().replace(/\s/g, "");
+        console.log(selection);
         if (selection == "rock") return ROCK;
         if (selection == "paper") return PAPER;
         if (selection == "scissors") return SCISSORS;
@@ -18,30 +19,67 @@ function computerPlay() {
     return choice;
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerChoice = parseChoice(playerSelection);
-    computerChoice = parseChoice(computerSelection);
+function playRound(e) {
+    playerChoice = parseChoice(e.target.textContent);
+    computerChoice = computerPlay();
     if (playerChoice == "Error" || computerChoice == "Error") return "Faulty game state";
     switch (playerChoice) {
         case ROCK:
-            if (computerChoice == playerChoice) return "Tie!";
-            if (computerChoice == PAPER) return "Lose!";
-            if (computerChoice == SCISSORS) return "Win!";
+            if (computerChoice == playerChoice) tally("Tie!");
+            if (computerChoice == PAPER)  tally("Lose!");
+            if (computerChoice == SCISSORS) tally("Win!");
             break;
         case PAPER:
-            if (computerChoice == playerChoice) return "Tie!";
-            if (computerChoice == SCISSORS) return "Lose!";
-            if (computerChoice == ROCK) return "Win!";
+            if (computerChoice == playerChoice) tally("Tie!");
+            if (computerChoice == SCISSORS) tally("Lose!");
+            if (computerChoice == ROCK) tally("Win!");
             break;
         case SCISSORS:
-            if (computerChoice == playerChoice) return "Tie!";
-            if (computerChoice == ROCK) return "Lose!";
-            if (computerChoice == PAPER) return "Win!";
+            if (computerChoice == playerChoice) tally("Tie!");
+            if (computerChoice == ROCK) tally("Lose!");
+            if (computerChoice == PAPER) tally("Win!");
             break;
     }
 
 }
+function tally(result) {
+    const playerScore = document.querySelector('.score.player');
+    const computerScore = document.querySelector('.score.computer');
+    if (result == 'Win!') {
+        playerScore.textContent = +playerScore.textContent + 1;
+    } else if (result == 'Lose!') {
+        computerScore.textContent = +computerScore.textContent + 1;
+    }
+    if (playerScore.textContent == 5) {
+        score('player');
 
+    } else if (computerScore.textContent == 5) {
+        score('computer');
+    }
+}
+
+function score(winner) {
+    const score = document.createElement('div');
+    score.classList.add('results');
+    if (winner == 'player') {
+        score.style.cssText = 'color: blue; background: yellow;';
+        score.textContent = 'You win! Congratulations! Click \'Reset\' again!';
+    } else { 
+        score.style.cssText = 'color: red; background: orange;';
+        score.textContent = 'You lost! Try again! Click \'Reset\' again!';
+    }
+    document.querySelector('.scoreboard').appendChild(score);
+}
+
+function resetGame() {
+    const playerScore = document.querySelector('.score.player');
+    const computerScore = document.querySelector('.score.computer');
+    playerScore.textContent = 0;
+    computerScore.textContent = 0;
+    const scoreWinner = document.querySelector('.results');
+    if (scoreWinner) scoreWinner.remove();
+}
+/*
 function game() {
     let wins = 0;
     let losses = 0;
@@ -57,4 +95,9 @@ function game() {
     else if (losses > wins) return "Loser!";
     return "Tied!";
 }
+*/
+
+const buttons = document.querySelectorAll('.playerinput');
+buttons.forEach(button => button.addEventListener('click', playRound));
+
 
